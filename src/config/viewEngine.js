534503c1d711dc5795102b1f0f2ac4567ca69e3e
OanhLv <laioanh961@gmail.com>
2024-04-02 08:02:02 +0700

    using image and color

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,6 +1,7 @@
 import Express from "express";
 
 const configViewEngine = (app) => {
+    app.use(Express.static('./src/public'))
     app.set("view engine", "ejs");
     app.set("views", "./src/views")
 }
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import configViewEngine from './config/viewEngine';
+require('dotenv').config();
+
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080;
+console.log('>>>check port: ', port)
 
 configViewEngine(app);
 
