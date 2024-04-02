e16dcdbb1fa982f0a59f032ec474b3a156c53192
OanhLv <laioanh961@gmail.com>
2024-04-02 07:32:03 +0700

    how to use EJS

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import configViewEngine from './config/viewEngine';
+const app = express()
+const port = 8080
+
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
